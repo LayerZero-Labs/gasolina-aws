fbe082ca46c5dc3340cb91f03d248076a344d712
Hugo Queirós <hugoqueiros@192.168.1.133>
2025-05-29 01:12:20 +0100

    update version

--- cdk/gasolina/config/index.ts
+++ cdk/gasolina/config/index.ts
@@ -1,5 +1,5 @@
 // Canary implementation
-const GASOLINA_REPO = 'canaryprotocol/layerzero-dvn@sha256:a68a85737b16b8313b4a6b094a782a80f8a653957ac899e9d9cd66e3aef78014';
+const GASOLINA_REPO = 'canaryprotocol/layerzero-dvn@sha256:03e602a1068894a6d597d90f3a023194a06d4f17ca3f97f75ea369decf41019d';
 // Layer0 implementation was us-east1-docker.pkg.dev/lz-docker/gasolina/gasolina@sha256:e1c37062ff5e2f61dc69c7b405d3851a3db042c450d8410caf935bf426c7cdf0
 
 export const CONFIG: {
