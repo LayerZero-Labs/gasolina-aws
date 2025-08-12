695be58ef06e556b4af7d6d9f8e666d302165d46
MohammadChavosh <mohammad.chavosh@gmail.com>
2025-08-12 14:42:05 -0400

    Add Aptos,Movement,Initia support for configChangePayload scripts (#22)
    * Add Aptos,Movement,Initia support for configChangePayload scripts
    
    * do the split for the solana signatures

--- scripts/configChangePayloads/createAddOrRemoveSignerSignatures.ts
+++ scripts/configChangePayloads/createAddOrRemoveSignerSignatures.ts
@@ -84,7 +84,11 @@ const main = async () => {
             )
 
             const signatures = await getSignatures(keyIds, hash, chainName)
-            const signaturesPayload = getSignaturesPayload(signatures, quorum)
+            const signaturesPayload = getSignaturesPayload(
+                signatures,
+                quorum,
+                chainName,
+            )
 
             results[chainName] = {
                 args: {
--- scripts/configChangePayloads/createSetQuorumSignatures.ts
+++ scripts/configChangePayloads/createSetQuorumSignatures.ts
@@ -77,6 +77,7 @@ const main = async () => {
             const signaturesPayload = getSignaturesPayload(
                 signatures,
                 oldQuorum,
+                chainName,
             )
 
             results[chainName] = {
